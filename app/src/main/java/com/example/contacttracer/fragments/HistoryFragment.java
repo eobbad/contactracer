07ba39ab90873e5f157e94feb066e38de3a1acc0
eobbad <35933488+eobbad@users.noreply.github.com>
2020-07-31 10:45:40 -0700

    updated history view

--- app/src/main/java/com/example/contacttracer/fragments/HistoryFragment.java
+++ app/src/main/java/com/example/contacttracer/fragments/HistoryFragment.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -34,6 +35,8 @@ import com.parse.ParseGeoPoint;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import org.w3c.dom.Text;
+
 import java.util.List;
 
 public class HistoryFragment extends Fragment{
@@ -41,6 +44,7 @@ public class HistoryFragment extends Fragment{
     //I have to use a mapView instead of a
     private MapView mMapView;
     private GoogleMap googleMap;
+    private TextView tvMessage;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -50,6 +54,8 @@ public class HistoryFragment extends Fragment{
 
         View view = inflater.inflate(R.layout.fragment_history, container, false);
         mMapView = (MapView) view.findViewById(R.id.mapView);
+        tvMessage = (TextView) view.findViewById(R.id.tvMessage);
+
         mMapView.onCreate(savedInstanceState);
         mMapView.onResume(); // needed to get the map to display immediately
         try {
@@ -89,6 +95,7 @@ public class HistoryFragment extends Fragment{
                         if (e == null) {
                             // avoiding null pointer
                             ParseUser closestUser = ParseUser.getCurrentUser();
+                            tvMessage.setText("There are " + nearUsers.size() + " users nearby");
                             // set the closestUser to the one that isn't the current user
                             for(int i = 0; i < nearUsers.size(); i++) {
                                 if(!nearUsers.get(i).getObjectId().equals(ParseUser.getCurrentUser().getObjectId())) {
@@ -114,6 +121,7 @@ public class HistoryFragment extends Fragment{
                 googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
             }
         });
+
         // Lookup the swipe container view
         return view;
     }
