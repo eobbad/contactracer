9f11df015549467133ecc52323639b9b8969b289
eobbad <35933488+eobbad@users.noreply.github.com>
2020-07-30 18:04:20 -0700

    Finished styling login page

--- app/src/main/java/com/example/contacttracer/WarningsAdapter.java
+++ app/src/main/java/com/example/contacttracer/WarningsAdapter.java
@@ -1,5 +1,6 @@
 package com.example.contacttracer;
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
