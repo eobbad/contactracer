eeb39c4b98ce3b5ea9ca3889d2a3f6c14d932dd6
eobbad <35933488+eobbad@users.noreply.github.com>
2020-08-04 22:30:11 -0700

    Added animation feature to the popup window, now it pops up from the bottom of the screen, and exits the same way

--- app/src/main/java/com/example/contacttracer/WarningsAdapter.java
+++ app/src/main/java/com/example/contacttracer/WarningsAdapter.java
@@ -66,6 +66,7 @@ public class WarningsAdapter extends RecyclerView.Adapter<WarningsAdapter.ViewHo
                 boolean focusable = true; // lets taps outside the popup also dismiss it
                 final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
 
+                popupWindow.setAnimationStyle(R.style.Animation_Design_BottomSheetDialog);
                 // show the popup window
                 // which view you pass in doesn't matter, it is only used for the window tolken
                 popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
